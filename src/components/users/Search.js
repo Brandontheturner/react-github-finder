1e722f5f1d8574af5bff9102c9eade10c867e1fd
brandotheturner <brandontheturner@gmail.com>
2019-08-03 13:31:20 -0500

    onSubmit and onChange

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: ""
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
+
+  //use setState in class based components
+  //setting state to whatever is typed into search box
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
-        <form className='form'>
+        <form onSubmit={this.onSubmit} className='form'>
           <input
             type='text'
             name='text'
             placeholder='Search Users..'
             value={this.state.text}
-            onChange=
+            onChange={this.onChange}
           />
           <input
             type='submit'
