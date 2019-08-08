d7443a371466a6698cd9fa81a84672b5f11b8132
brandotheturner <brandontheturner@gmail.com>
2019-08-07 21:26:27 -0500

    clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,11 +7,14 @@ export class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onSubmit = e => {
     e.preventDefault();
+
     this.props.searchUsers(this.state.text);
     this.setState({ text: "" });
   };
@@ -21,6 +24,9 @@ export class Search extends Component {
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    // Destructuring
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -37,6 +43,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
