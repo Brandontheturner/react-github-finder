45e86fd719412bdc24c5f1d2eb3d6ebc99bfc521
brandotheturner <brandontheturner@gmail.com>
2019-08-08 14:04:50 -0500

    alert box

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -14,9 +14,12 @@ export class Search extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   //use setState in class based components
