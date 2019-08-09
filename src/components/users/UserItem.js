d8a2060b41eb0b7f95dbb11ae57d5d34d55d06c2
brandotheturner <brandontheturner@gmail.com>
2019-08-09 13:01:03 -0500

    user component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 //destructuring of props in parameter section of function declaration//
 //user prop being passed in so need to import propTypes//
@@ -15,9 +16,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       <h2>{login}</h2>
 
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
