36e5c9e1fd68fb6076b8b59794723f1558500b6d
brandotheturner <brandontheturner@gmail.com>
2019-08-11 21:56:11 -0500

    implementing context

---
+++ src/context/github/githubContext.js
@@ -0,0 +1,7 @@
+//purpose of file is to intialize new context
+
+import { createContext } from "react";
+
+const githubContext = createContext();
+
+export default githubContext;
---
+++ src/context/types.js
@@ -0,0 +1,9 @@
+//types are just variable of strings that you can call to change state within reducer
+
+export const SEARCH_USERS = "SEARCH_USERS";
+export const GET_USERS = "GET_USERS";
+export const CLEAR_USERS = "CLEAR_USERS";
+export const GET_REPOS = "GET_REPOS";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
